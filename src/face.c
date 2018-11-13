dafd0fa0dca98c08e4e1825f4393f69e499cc262
ErikBoesen <me@erikboesen.com>
2018-11-13 18:54:33 -0500

    Add instructions layer

--- src/face.c
+++ src/face.c
@@ -1,28 +1,32 @@
 #include <pebble.h>
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_instructions_layer;
 
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
   // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(
-      GRect(0, 5, bounds.size.w, 50));
-
-  // Improve the layout to be more like a watchface
+  s_time_layer = text_layer_create(GRect(0, 0, bounds.size.w, 45));
   //text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  // Add to window
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
+  s_instructions_layer = text_layer_create(GRect(0, 45, bounds.size.w, 300));
+  text_layer_set_text_color(s_instructions_layer, GColorBlack);
+  text_layer_set_text(s_instructions_layer, "Press and hold select (right center) button for 1 second to open the drive coaching app, then press again when the match starts to run the timer.");
+  text_layer_set_font(s_instructions_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_instructions_layer, GTextAlignmentLeft);
+  layer_add_child(window_layer, text_layer_get_layer(s_instructions_layer));
 }
 
 static void main_window_unload(Window *window) {
-  // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_instructions_layer);
 }
 
 static void update_time() {
@@ -32,8 +36,7 @@ static void update_time() {
 
   // Write the current hours and minutes into a buffer
   static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
